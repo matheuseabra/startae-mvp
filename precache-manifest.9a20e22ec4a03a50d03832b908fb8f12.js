c3c5b019308318ef94294b22ccf39e60eaada314
Matheus <matheusvieiracoelho@gmail.com>
2019-01-11 14:00:19 -0200

    Updates

---
+++ precache-manifest.9a20e22ec4a03a50d03832b908fb8f12.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc5a20d924933ab2fdb7849f87daf1e0",
+    "url": "/startae-mvp/static/media/logo-type.dc5a20d9.svg"
+  },
+  {
+    "revision": "c05a6061849f2145a904",
+    "url": "/startae-mvp/static/css/main.f225a1d8.chunk.css"
+  },
+  {
+    "revision": "ea8a28b5db98480b7f7b",
+    "url": "/startae-mvp/static/js/1.ea8a28b5.chunk.js"
+  },
+  {
+    "revision": "1e87fe9ef714c6abe4e6",
+    "url": "/startae-mvp/static/js/runtime~main.1e87fe9e.js"
+  },
+  {
+    "revision": "3009a01a4f9631139d0adcbffb2f41fa",
+    "url": "/startae-mvp/static/media/logo-symbol.3009a01a.svg"
+  },
+  {
+    "revision": "c05a6061849f2145a904",
+    "url": "/startae-mvp/static/js/main.c05a6061.chunk.js"
+  },
+  {
+    "revision": "1ff45197615b5dbdaf0d8f55a20420d5",
+    "url": "/startae-mvp/static/media/logo-twitter.1ff45197.svg"
+  },
+  {
+    "revision": "dea306f42204a5280bbd7714bbd1aaaf",
+    "url": "/startae-mvp/static/media/heart.dea306f4.svg"
+  },
+  {
+    "revision": "ac51ad8a36d5bab6da11d454bcd06561",
+    "url": "/startae-mvp/static/media/ProximaNova-Light.ac51ad8a.otf"
+  },
+  {
+    "revision": "efc2820473a48f510ce341184e5c2daf",
+    "url": "/startae-mvp/static/media/ProximaNova-LightItalic.efc28204.otf"
+  },
+  {
+    "revision": "01dcb5e615f6b44348ce59721449a19f",
+    "url": "/startae-mvp/index.html"
+  }
+];
--- src/services/api.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({ baseURL: 'https://gist.githubusercontent.com/sales/f961f967c6668c7c1c3ed565e3bf94e8/raw/71168b72d9a3a199bd1de01026b5031875f6d432'});
-
-export default api;
